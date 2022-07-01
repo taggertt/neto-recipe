d6a9af9c1188710409edcf78c3cbf9373a946da5
taggertt <taggertt@mail.ru>
2022-07-01 21:47:55 +0500

    check recipe

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+    System.out.println("Привет, Нетология!");
+    System.out.println("2+3");
+    System.out.println(2+3);
+
+   int speed = 17;
+   int x = speed + 3;
+
+   double d = 6.5;
+   String name = "taggertt";
+
+    System.out.println(name + " ехал со скоростью " + speed);
+        }
+}
